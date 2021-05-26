a5e9cc39fe43cf89d577178f7c25c1ba464574cc
krummja <crumja@uga.edu>
2021-05-25 20:55:11 -0400

    Anathema-7 Implement RectView

---
+++ anathema/views/rect_view.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import Tuple, List, Dict, Any, TYPE_CHECKING, Optional
+
+from anathema.views.view import View
+from anathema.prepare import CONSOLE
+
+if TYPE_CHECKING:
+    from anathema.views.layout import Layout
+    from anathema.screen import Screen
+    from morphism import Rect  # type: ignore
+
+
+class RectView(View):
+
+    def __init__(
+            self,
+            fg: Tuple[int, int, int] = (255, 255, 255),
+            bg: Tuple[int, int, int] = (21, 21, 21),
+            fill: bool = False,
+            style: str = 'single',
+            screen: Optional[Screen] = None,
+            layout: Optional[Layout] = None,
+            subviews: Optional[List[View]] = None,
+            frame: Optional[Rect] = None
+        ) -> None:
+        super().__init__(screen, layout, subviews, frame)
+        self.fg = fg
+        self.bg = bg
+        self.fill = fill
+        self.style = style
+
+    def draw(self) -> None:
+        CONSOLE.draw_frame(self.bounds, fg=self.fg, bg=self.bg)
